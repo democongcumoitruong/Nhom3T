dd41d894a0178fb06c4721d0d036662b80adc6f6
Nhat Thuong <nn.thuong@hutech.edu.vn>
2023-03-27 00:26:45 +0700

    tao project, giao dien dang nhap-dang ky

---
+++ AppBanDoAn/app/src/main/java/org/o7planning/appbandoan/DangKy.java
@@ -0,0 +1,14 @@
+package org.o7planning.appbandoan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangKy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
