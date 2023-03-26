4541b55f5fcbeab67a4252a08f5facc68609e2d5
Nhat Thuong <nn.thuong@hutech.edu.vn>
2023-03-27 00:40:32 +0700

    giao dien dang nhap-dang ky

--- AppBanDoAn/app/src/main/java/org/o7planning/appbandoan/MainActivity.java
+++ AppBanDoAn/app/src/main/java/org/o7planning/appbandoan/MainActivity.java
@@ -2,13 +2,30 @@ package org.o7planning.appbandoan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btndangnhap,btndangky;
+    private EditText editTextsdt,editTextmk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btndangky = (Button) findViewById(R.id.dangky1);
+        btndangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DangKy.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
